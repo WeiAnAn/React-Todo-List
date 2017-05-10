9ab04ec7e6e3adda082ed205b9c691d921bd25a1
Wei-An <j22461615@gmail.com>
2017-05-10 15:01:44 +0800

    add production config

---
+++ webpack.config.prod.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        "./index.jsx",
+    ],
+    output: {
+        compress: true,
+        filename: 'bundle.js',
+        path: path.resolve(__dirname,'dist'),
+    },
+    context: path.resolve(__dirname,''),
+    module:{
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },{
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
+            }]
+    },
+}
